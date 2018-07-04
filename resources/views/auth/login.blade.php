9c41e534074690888dd7bfb5c445e55cf8908388
masahiro.ozaki <masahiro.b.ozaki@rakuten.com>
2018-07-04 12:20:35 +0000

    user login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,18 +2,13 @@
 
 @section('content')
     <div class="text-center">
-        <h1>Sign up</h1>
+        <h1>Log in</h1>
     </div>
 
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
 
-            {!! Form::open(['route' => 'signup.post']) !!}
-                <div class="form-group">
-                    {!! Form::label('name', 'ニックネーム') !!}
-                    {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
-                </div>
-
+            {!! Form::open(['route' => 'login.post']) !!}
                 <div class="form-group">
                     {!! Form::label('userid', 'ユーザーID') !!}
                     {!! Form::text('userid', old('userid'), ['class' => 'form-control']) !!}
@@ -24,13 +19,10 @@
                     {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
 
-                <div class="form-group">
-                    {!! Form::label('password_confirmation', 'パスワード(確認)') !!}
-                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
-                </div>
-
-                {!! Form::submit('Sign up', ['class' => 'btn btn-primary btn-block']) !!}
+                {!! Form::submit('Log in', ['class' => 'btn btn-primary btn-block']) !!}
             {!! Form::close() !!}
+
+            <p>New user? {!! link_to_route('signup.get', 'Sign up now!') !!}</p>
         </div>
     </div>
 @endsection
